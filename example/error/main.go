e23b51240d4cf37bead012e376026acb68a8dd30
Alexander tegtmeier <alexandertegtmeier@proton.me>
2025-07-06 10:34:56 +0200

    add example service demonstrating error wrapping and tagging

---
+++ example/error/main.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"context"
+	"github.com/aledantee/ae"
+	"github.com/aledantee/service"
+)
+
+func main() {
+	svc := service.Service{
+		Name:    "error",
+		Version: "0.0.1",
+		Init: func(ctx context.Context) error {
+			cause := ae.New().
+				Tag("component1").
+				Hint("do the thing").
+				Msg("failed")
+
+			return ae.Wrap("some error", cause)
+		},
+		Run: func(svc *service.Service) error {
+			panic("unreachable")
+		},
+		Shutdown: func(ctx context.Context) error {
+			panic("unreachable")
+		},
+	}
+
+	svc.ExecuteExit()
+}
